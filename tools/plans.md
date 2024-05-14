eebd8aee0cbcf1bfe3aa97c616504bc2ca45ac99
GenericRikka <rikka.goering@outlook.de>
2024-05-14 04:35:42 +0200

    Enhanced CLI of rcd-to-openrc.c

--- tools/plans.md
+++ tools/plans.md
@@ -1,1 +1,5 @@
-On this branch, i want to create a tool to automatically convert rc.d scripts to openrc, since the conversion follows a simple algorithm, it should not be too difficult to implement and will help me to get used with C again, so here goes nothing
+On this branch, i want to create a tool to automatically convert rc.d scripts to openrc, since the conversion follows a simple algorithm, it should not be too difficult to implement and will help me to get used with C again, so here goes nothing.
+
+After some time i am able to say, that this really is a good project to get back into C, i am a lot more confident about my C coding abilities than before this project. That being said in hindsight it was not that good of an idea to use C instead of C++, due to probably more maintenance effort, since the code to achieve the same result is longer in C than in C++
+So when i am done developing this project in C, i will most likely start to convert it into C++ afterwards. This will also be a good practice, to in turn get into C++ again. At least if i don't just come up with a new project, but who knows.
+
--- tools/rcd-to-openrc.c
+++ tools/rcd-to-openrc.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
 
 #define FPATH_LIMIT 500 /* maximum length for file paths */
 #define MAX_ROWS 1000 /* maximum of rows to be expected in the script */
 
-int read(char inputloc[], char outputloc[]); /* a small function to read from a file and then call another function with read data as argument */
-int convert(char* data, long size); /* main thread during the conversion of the script. It calls sub functions to modify different parts */
-int write(char outputloc[], char* data); /* a small function to write to a file */
-int delete(long a, long b, char* data); /* a function that deletes the section from a to b in data. one of the functions called by convert */
-int extract(char* pattern, char* delim, char* data, char* extract); /* a function which extracts the value found after pattern and before delim from data and saves it to extract. one of the functions called by convert */
-int insert(char* insert, long pos, char* data); /* this function inserts insert at pos in data. one of the functions called by convert */
-int replace(char target[], char phrase[], char* data); /* a function to replace target with phrase inside data. one of the functions called by convert */
-int cmdextract(char* data, char* extractpre, char* extractpost);
+void myread(char inputloc[], char outputloc[]); /* a small function to read from a file and then call another function with read data as argument */
+void convert(char* data, size_t size); /* main thread during the conversion of the script. It calls sub functions to modify different parts */
+void mywrite(char outputloc[], char* data); /* a small function to write to a file */
+void delete(size_t a, size_t b, char* data); /* a function that deletes the section from a to b in data. one of the functions called by convert */
+void extract(char* pattern, char* delim, char* data, char* extract); /* a function which extracts the value found after pattern and before delim from data and saves it to extract. one of the functions called by convert */
+void myinsert(char* insert, size_t pos, char* data); /* this function inserts insert at pos in data. one of the functions called by convert */
+void replace(char target[], char phrase[], char* data); /* a function to replace target with phrase inside data. one of the functions called by convert */
+void cmdextract(char* data, char* extractpre, char* extractpost);
 char* strrev(char* str);
+void print_progress(char *title1, char *title2, int count, int max, int *spinstore);
+void test_progress();
 int main();
 
 
@@ -51,28 +55,28 @@ int main(){
 	getchar();
 	printf("Now, please enter a path for the output file:\n");
 	scanf("%24[^\n]", &outlocation);
-	read(inlocation, outlocation);
-	printf("After read.");
+	myread(inlocation, outlocation);
+	printf(" Done!");
         //write(outlocation, "test");
 	return 0;
 }
 
-int read(char inputloc[FPATH_LIMIT], char outputloc[FPATH_LIMIT]){ //Tested. Works.
+void myread(char inputloc[FPATH_LIMIT], char outputloc[FPATH_LIMIT]){ //Tested. Works.
 	FILE *file;
 	char *data;
 	long size;
 
 	//printf("Read initialized.");
 	file = fopen(inputloc, "rb");
-	if(! file) fputs("[ERROR] Unable to open file.",stderr),perror(inputloc),printf("Unable to open file.\n"),exit(1);
+	if(! file) fputs("[ERROR] Unable to open file.",stderr),perror(inputloc),printf("Unable to open file.\n"),exit;
 	fseek(file, 0L, SEEK_END);
 	size = ftell(file);
 	rewind(file);
 
 	data = malloc(1 * size + 1); 
-	if(!data) fclose(file),fputs("[ERROR] Failed to allocate memory. Bailing out.",stderr),perror("malloc fail"),printf("[ERROR] Failed to allocate memory.\n"),exit(1);
+	if(!data) fclose(file),fputs("[ERROR] Failed to allocate memory. Bailing out.",stderr),perror("malloc fail"),printf("[ERROR] Failed to allocate memory.\n"),exit;
 	if(1 != fread(data, size, 1, file))
-		fclose(file),free(data),fputs("[ERROR] Read Operation from file failed.",stderr),perror("read fail"),printf("[ERROR] Read operation failed\n"),exit(1);
+		fclose(file),free(data),fputs("[ERROR] Read Operation from file failed.",stderr),perror("read fail"),printf("[ERROR] Read operation failed\n"),exit;
 	fread(data, size, 1, file);
 	fclose(file);
 	int i;
@@ -81,10 +85,9 @@ int read(char inputloc[FPATH_LIMIT], char outputloc[FPATH_LIMIT]){ //Tested. Wor
 	convert(data, size);
 	//write(outputloc,data);
 	free(data);
-	return(0);
 }
 
-int convert(char* data, long size){ 
+void convert(char* data, size_t size){ 
 	//int i;
 	//printf("Recieved:\n");
 	//for(i = 0; i <= size; i++) printf("%c",data[i]);
@@ -129,20 +132,64 @@ int convert(char* data, long size){
 	free(target);
 	free(phrase);*/
 
-	char* extractpre;
+	//This part was used to test cmdextract()
+	/*char* extractpre;
 	char* extractpost;
 	extractpre = malloc(sizeof(char)*40);
 	extractpost = malloc(sizeof(char)*40);
 	cmdextract(data,extractpre,extractpost);
 	printf("Function name: %s\nFunction command: %s",extractpre,extractpost);
 	free(extractpre);
-	free(extractpost);
-	return(0);
-}	
+	free(extractpost);*/
 
-int delete(long a, long b, char* data){ //Tested. Works.
-	long size = strlen(data);
-	long d = b - a;
+	test_progress();
+}
+
+void test_progress(){
+	size_t num_secs = 0, max_secs = 50;
+	printf("%s\n", "");
+	char title1[] = "/etc/rc.d/netif";
+	char title2[] = "/etc/init.d/netif";
+	int spinstore;
+	spinstore = 0;
+	for ( ; num_secs < max_secs; num_secs++){
+		usleep(50000);
+		print_progress(title1, title2, num_secs, max_secs, &spinstore);
+	}
+}
+
+void print_progress(char *title1, char *title2, int count, int max, int *spinstore){ //Remember to convert longs that hold array counts into size_t
+	const char prefix[] = " [";
+	const char suffix[] = "]     ";
+	const size_t prefix_length = sizeof(prefix) - 1;
+	const size_t suffix_length = sizeof(suffix) - 1;
+	char *buffer = malloc(max + prefix_length + suffix_length + 1);
+	size_t i;
+	strcpy(buffer, prefix);
+	for ( i = 0; i < max; i++ ){
+		buffer[prefix_length + i] = i <= count ? '#' : ' '; //Remember to change # to pretty unicode/ascii symbol
+	}
+	strcpy(&buffer[prefix_length + i], suffix);
+	
+	char *spin[5];
+	spin[0] = "-->  -->  ";
+	spin[1] = " -->  --> ";
+	spin[2] = "  -->  -->";
+	spin[3] = ">  -->  --";
+	spin[4] = "->  -->  -";
+	int spinmax = 4;
+	
+	printf("\r\b\b\b\r%s\b\b\b\b\b Converting: %s [%s] %s", buffer, title1, spin[*spinstore], title2);
+	fflush(stdout);
+	free(buffer);
+	if(*spinstore == spinmax) *spinstore = 0;
+	else if(*spinstore < spinmax && *spinstore >= 0) *spinstore = *spinstore + 1;
+	else printf("[ERROR] Illegal spinner state.\n"), fputs("[ERROR] Illegal spinner state.",stderr), perror("Illegal spinner state"), exit;
+}
+
+void delete(size_t a, size_t b, char* data){ //Tested. Works.
+	size_t size = strlen(data);
+	size_t d = b - a;
 	char *buffer;
 	buffer = malloc(sizeof(char)*size); 
 	int i;
@@ -154,67 +201,61 @@ int delete(long a, long b, char* data){ //Tested. Works.
 	}
 	for(i = 0; i < size+1; i++) data[i] = buffer[i];
 	free(buffer);
-	return(0);
 }
 
-int extract(char* pattern, char* delim, char* data, char* extract){ //Tested. Works.
-	int psz = strlen(pattern);
-	int desz = strlen(delim);
-	long dasz = strlen(data);
+void extract(char* pattern, char* delim, char* data, char* extract){ //Tested. Works.
+	size_t psz = strlen(pattern);
+	size_t desz = strlen(delim);
+	size_t dasz = strlen(data);
 	char *result1;
 	result1 = strstr(data,pattern);
 	long l;
 	for(l = psz; result1[l]!= delim[0]; l++) extract[l-psz] = result1[l];
 	printf("%s",extract);
-	return(0);
 }
 
-int cmdextract(char* data, char* extractpre, char* extractpost){ //Tested. Works.
+void cmdextract(char* data, char* extractpre, char* extractpost){ //Tested. Works.
 	//For the *_cmd="command" values an extra function is needed to extract both the * value and the command values
 	char pattern[] = "_cmd=\"";
 	char delimpre[] = "\n";
 	char delimpost[] = "\"";
 	char* ptr;
 	ptr = strstr(data,pattern);
-	long l;
+	size_t l;
 	for(l = 0; ptr[l] != delimpre[0]; l--) extractpre[l * -1] = ptr[l];
-	long m;
+	size_t m;
 	strrev(extractpre);
 	for(m = 0; ptr[m + strlen(pattern)] != delimpost[0]; m++) extractpost[m] = ptr[m + strlen(pattern)];
-	return(0);
 }
 
-int insert(char* insert, long pos, char* data){ //Tested. Works.
+void myinsert(char* insert, size_t pos, char* data){ //Tested. Works.
 	char* buffer;
 	buffer = malloc(sizeof(char)*strlen(data));
-	long insize = strlen(insert);
-	long dsize = strlen(data);
-	long l;
+	size_t insize = strlen(insert);
+	size_t dsize = strlen(data);
+	size_t l;
 	for(l = 0; l < pos; l++) buffer[l] = data[l];
 	for(l = 0; l < insize; l++) buffer[pos + l] = insert[l];
 	for(l = pos; l <= dsize; l++) buffer[l+insize] = data[l];
 	strcpy(data,buffer);
 	free(buffer);
-	return(0);
 }
 
-int replace(char target[], char phrase[], char* data){ //Tested. Works.
+void replace(char target[], char phrase[], char* data){ //Tested. Works.
 	char* pos;
-	long tsize = strlen(target);
+	size_t tsize = strlen(target);
 	pos = strstr(data,target);
-	long l;
+	size_t l;
 	delete(0,tsize,pos);
-	insert(phrase,0,pos);
+	myinsert(phrase,0,pos);
 	printf("%s",data);
-	return(0);
 }
 
-int write(char outputloc[1000], char* data){ //Tested. Works.
+void mywrite(char outputloc[1000], char* data){ //Tested. Works.
 	FILE *fptr;
 	fptr = fopen(outputloc, "w");
 	fprintf(fptr, data);
 	fclose(fptr);
-	return(0);
 }
 
 char* strrev(char* str) //Reverses a string
